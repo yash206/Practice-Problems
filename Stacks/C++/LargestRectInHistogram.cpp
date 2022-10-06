d59f555967f8dcf7d65f14444e4c65005281a464
Lakshay <englakshaysk@gmail.com>
2022-10-06 16:38:23 +0530

    Added a few std. montonic stack problem solutions

---
+++ Stacks/C++/LargestRectInHistogram.cpp
@@ -0,0 +1,59 @@
+// C++ program to find largest rectangle area in histogram using stack
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int largestRectangleArea(vector<int> &heights)
+{
+    int n = heights.size();
+    vector<int> nsl(n), psl(n);
+
+    stack<int> s;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && heights[s.top()] >= heights[i])
+        {
+            s.pop();
+        }
+        nsl[i] = s.empty() ? n : s.top();
+        s.push(i);
+    }
+
+    while (!s.empty())
+    {
+        s.pop();
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!s.empty() && heights[s.top()] >= heights[i])
+        {
+            s.pop();
+        }
+        psl[i] = s.empty() ? -1 : s.top();
+        s.push(i);
+    }
+
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        ans = max(ans, (nsl[i] - psl[i] - 1) * (heights[i]));
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> heights;
+    heights.push_back(2);
+    heights.push_back(1);
+    heights.push_back(5);
+    heights.push_back(6);
+    heights.push_back(2);
+    heights.push_back(3);
+    cout << largestRectangleArea(heights) << endl;
+    return 0;
+}
---
+++ Stacks/C++/StockSpan.cpp
@@ -0,0 +1,46 @@
+// C++ program  to calculate stock span values using stack
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> calculateSpan(int arr[], int n)
+{
+    // Your code here
+    stack<int> s;
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        while (!s.empty() and arr[s.top()] <= arr[i])
+            s.pop();
+
+        if (s.empty())
+            ans.push_back(i + 1);
+        else
+        {
+            int top = s.top();
+            ans.push_back(i - top);
+        }
+        s.push(i);
+    }
+
+    return ans;
+}
+
+// A utility function to print elements of array
+void printArray(vector<int> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+}
+
+// Driver code
+int main()
+{
+    int price[] = {10, 4, 5, 90, 120, 80};
+    int n = sizeof(price) / sizeof(price[0]);
+    int S[n];
+
+    vector<int> arr = calculateSpan(price, n);
+    printArray(arr);
+
+    return 0;
+}
