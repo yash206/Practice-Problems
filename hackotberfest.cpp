e197c882d24b5decb74768b2c53c61191f815c22
Veeno Rai <101185711+Veeno-Rai@users.noreply.github.com>
2022-10-08 01:42:54 +0530

    Update hackotberfest.cpp
    Output of given code will be :
         Cycle Found

--- hackotberfest.cpp
+++ hackotberfest.cpp
@@ -1,55 +1,70 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_set>
 using namespace std;
-class node{
-    public:
-    int data;
-    node* next;
-    node(int val)
-    {
-        data = val;
-        next=NULL;
-       
-    }
-};
-// detect loop using floyd's cycle algorithm
-// this achieved by creating two ptr slow and fast where these two pointer point to same node loop is found
-int detectLoop(node* &list)
-{
-    node *slow_p = list, *fast_p = list;
  
-    while ((fast_p !=NULL)&& (fast_p->next!=NULL)) {
-        slow_p = slow_p->next;
-        fast_p = fast_p->next->next;
-        if (slow_p == fast_p) {
-            return 1;
+// A Linked List Node
+struct Node
+{
+    int data;
+    Node* next;
+};
+ 
+// function to create linked list
+void push(Node*& head, int data)
+{
+    // create a new linked list node from the heap
+    Node* newNode = new Node;
+ 
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+}
+ 
+// Floyd’s cycle detection algorithm
+bool detectCycle(Node* head)
+{
+    // take two pointers – `slow` and `fast`
+    Node *slow = head, *fast = head;
+ 
+    while (fast && fast->next)
+    {
+        // move slow by one pointer
+        slow = slow->next;
+ 
+        // move fast by two pointers
+        fast = fast->next->next;
+ 
+        // if they meet any node, the linked list contains a cycle
+        if (slow == fast) {
+            return true;
         }
     }
-    return 0;
+    // we reach here if the slow and fast pointer does not meet
+    return false;
 }
-void insert_node(node* head,int val)
-{
-    node* Node=new node(val);
-    if(head==NULL)
-    {
-        head=Node;
-        return;
-    }
-    node* temp=head;
-     while(temp->next!=NULL)
-     {
-         temp=temp->next;
-
-     }
-    temp->next =Node;
-}
+ 
 int main()
 {
-    node* head=NULL;
-    insert_node(head,10);
-    insert_node(head,4);
-    insert_node(head,15);
-    insert_node(head,20);
-    if(detectLoop(head)) cout<<"1";
-    else cout<<"0";
+    // input keys
+    int keys[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(keys) / sizeof(keys[0]);
+ 
+    Node* head =NULL;
+    for (int i = n - 1; i >= 0; i--) {
+        push(head, keys[i]);
+    }
+    // insert cycle
+    head->next->next->next->next->next = head->next->next;
+ 
+    if (detectCycle(head)) {
+        cout << "Cycle Found";
+    }
+    else {
+        cout << "No Cycle Found";
+    }
     return 0;
 }
+/*
+Output::
+        Cycle Found
+*/
