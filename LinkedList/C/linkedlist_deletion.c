d422aaa2978a25653d6876cddfeb373973e3a51c
Balashivaram Ganesan <71939272+Balashivaram@users.noreply.github.com>
2022-10-07 15:13:48 +0530

    Update linkedlist_deletion.c
    Added output and problem statement for the program

--- LinkedList/C/linkedlist_deletion.c
+++ LinkedList/C/linkedlist_deletion.c
@@ -1,3 +1,8 @@
+/*Problem Statementrite 
+Write a C code to create a linked list where we need to get values for the linked list manually and then write a function to get the value from the user to delete 
+the value from the linked list and then wirte a function to print the values in the list */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -89,3 +94,43 @@ int main(){
         }
     }
 }
+
+/*Output
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:1
+Enter number to add to the list:1
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:1
+Enter number to add to the list:2
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:1
+Enter number to add to the list:3
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:3
+
+The values in the list are:1 2 3 
+
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:2
+Enter the number to delete:2
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:3
+
+The values in the list are:1 3
+
+1)Add element to list
+2)Delete element of our choice
+3)View list
+Enter your chocice:4 */
