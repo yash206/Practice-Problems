d52603881e7ae663e9c3528665cfb4d40ee232d3
Brian Chau <brian.chau@outlook.com>
2022-10-08 00:22:37 -0700

    Added problem statement and output

--- Arrays/Python/FirstMissingInteger.py
+++ Arrays/Python/FirstMissingInteger.py
@@ -1,3 +1,12 @@
+# Given an unsorted integer array nums, return the smallest missing positive integer.
+# You must implement an algorithm that runs in O(n) time and uses constant extra space.
+# Input: nums = [1,2,0]
+# Output: 3
+# Input: nums = [3,4,-1,1]
+# Output: 2
+# Input: nums = [7,8,9,11,12]
+# Output: 1
+
 def firstMissingPositive(nums: list) -> int:
     maximum=1
     numset=set(nums)
