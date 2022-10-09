997c1a36bcd6d9f79cda3c80499ac85a32c39535
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2022-10-09 11:39:20 +0530

    Update linkedlistsearch.cpp
    Added the  problem statement and the output

--- LinkedList/C++/linkedlistsearch.cpp
+++ LinkedList/C++/linkedlistsearch.cpp
@@ -1,15 +1,25 @@
+/*PROBLEM------
+CREATE A LINKED LIST AND 
+WRITE FUNCTIONS TO SEARCH FOR AN ELEMENT,
+BOTH RECURSIVELY AND ITERATIVELY AND COMPARE IT
+*/
+
+
 #include<iostream>
 
 using namespace std;
+int key;
 
 
+
+// CREATING STRUCTURE OF THE LINKED LIST
 
 struct node{
     int data;
     struct node *next;
 };
 
-struct node *first;
+struct node *first;    //POINTER TO FIRST NODE OF LINKED LIST
 
 
 
@@ -25,8 +35,8 @@ return p8;// since, return type is node pointer
     else{
     q=p8;
     p8=p8->next;
-  
-   
+
+
 
 }
 }
@@ -45,10 +55,10 @@ struct node *recurLsearch(struct node *p9,int key){
     }
 }
 
-
+//FUNCTION TO DISPLAY LINKED LIST RECURSIVELY
 void display2(struct node *p2){
     if(p2!=NULL){
-   
+
    cout<<p2->data<<endl;
    display2(p2->next);
 
@@ -57,7 +67,7 @@ void display2(struct node *p2){
 
 
 int main(){
-    //CREATING LINKED LIST
+    //CREATING LINKED LIST BY HELP OF ARRAY
     int arr[]={1,2,3,4,5,6};
    struct node *last,*temp;
    first=new node;
@@ -72,9 +82,37 @@ for(int i=1;i<6;i++){
     last->next=temp;
     last=temp;
    }
-  
-  
-  cout<<recurLsearch(first,3)<<endl;
+   
+   
+   
+  cout<<"linked list is "<<endl;
   display2(first);
+  cout<<"ENTER THE VALUE FOR FINDING IN LINKED LIST "<<endl;
+  cin>>key;
+  cout<<"----------------------------------"<<endl;
+  cout<<"The address of " <<key<<" by RECURSIVE search is ";
+  cout<<recurLsearch(first,key)<<endl;
+  cout<<"The address of " <<key<<" by ITERATIVE search is ";
+  cout<<lsearch(first,key)<<endl;
+  cout<<"----------------------------------"<<endl;
+
   return 0;
 }
+
+
+
+/* OUTPUT------------
+linked list is 
+1
+2
+3
+4
+5
+6
+ENTER THE VALUE FOR FINDING IN LINKED LIST
+4
+-------------------------------
+The address of 4 by RECURSIVE search is 0x1051aa0
+The address of 4 by ITERATIVE search is 0x1051aa0
+----------------------------------
+*/
