82ad97718687a3887243872d00e7e6931e9e574b
Atul <atul.ak1510@gmail.com>
2022-10-13 20:03:46 +0530

    Added Longest Substring Without Repeating Characters Problem

---
+++ Strings/C++/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,47 @@
+// Problem Link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Problem Statement : Given a string s, find the length of the longest substring without repeating characters.
+
+// Problem Code : 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(string str) 
+{
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  for (int i = 0; i < str.length(); i++) 
+  {
+    unordered_set < int > set;
+    for (int j = i; j < str.length(); j++) 
+    {
+      if (set.find(str[j]) != set.end()) 
+      {
+        maxans = max(maxans, j - i);
+        break;
+      }
+      set.insert(str[j]);
+    }
+  }
+  return maxans;
+}
+
+int main() 
+{
+  string str = "takeUforward";
+  cout << "The length of the longest substring without repeating characters is " << 
+  solve(str);
+  return 0;
+}
+
+// INPUT AND OUTPUT :
+
+/* Eg.1 : 
+Input: s = "bbbbb"
+Output: 1 */
+
+/* Input: s = "pwwkew"
+Output: 3*/
