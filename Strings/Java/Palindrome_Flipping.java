e78ed59218d606598160d1d41d99fb3c3c550b09
Anubhav Sonker <anubhav1546@gmail.com>
2022-10-15 17:59:57 +0530

    I have submitted the the solution for Palindrome flipping

--- Strings/Java/Palindrome_Flipping.java
+++ Strings/Java/Palindrome_Flipping.java
@@ -1,13 +1,9 @@
-
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
+public class Palindrome_Flipping {
+    public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		try{
@@ -36,5 +32,5 @@ class Codechef
 		catch(Exception e){
 		    
 		}
-	}
+	} 
 }
