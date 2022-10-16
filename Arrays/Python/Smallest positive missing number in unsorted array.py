3c72d7144ba1648845696cfe39035481ec27ae4e
Amit Kumar Gupta <91068161+17h4ck3r11@users.noreply.github.com>
2022-10-16 08:25:48 +0530

    Adding Problem statement and Output

--- Arrays/Python/Smallest positive missing number in unsorted array.py
+++ Arrays/Python/Smallest positive missing number in unsorted array.py
@@ -14,3 +14,11 @@ arr = [-2, 2, 1, -3, 2, 10, 0]
 length = len(arr)
 res = findNum(arr, length)
 print("The smallest positive missing number in the array is:",res)
+
+'''
+Problem Statement: You are given an array arr[] of N integers including 0. The task is to find the smallest positive number missing from the array.
+
+Example:
+arr = [-2, 2, 1, -3, 2, 10, 0]
+Output: The smallest positive missing number in the array is: 3
+'''
