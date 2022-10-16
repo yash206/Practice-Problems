a732289e7dbab48964c49e3b51f0ff7996081c97
Esben Volsgaard <esben.volsgaard@gmail.com>
2022-10-16 12:05:33 +0200

    add problem descriptions

--- Strings/PHP/php_implode.php
+++ Strings/PHP/php_implode.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Problem description
+ * 
+ * If you have an array that you would like to output without looping over it, eg. names or cars, you can use the implode() method.
+ * Implode() splits the items in the array up, and has a seperator, which is user defined, as the first parameter.
+ * 
+ */
+
 $myArray = [
     'BMW',
     'Audi',
--- Strings/PHP/php_trim.php
+++ Strings/PHP/php_trim.php
@@ -1,6 +1,13 @@
 <?php
 
-$stringToTrim = 'test php is awesome';
-$result = trim($stringToTrim, 'test'); // removes a specific word/character from a string, and returns the result.
+/**
+ * Problem description
+ * 
+ * If you have a string that is decided by the user eg. form sent to PHP file, then that user could've added whitespaces to the string.
+ * This isn't ideal if you'd like to add it to the database, therefore you can trim the whitespaces from the string.
+ */
 
-echo $result; // returns: "php is awesome"
+$stringToTrim = '          php is         cool          !';
+$result = trim($stringToTrim); // trims whitespaces from the string
+
+echo $result; // returns: "php is cool !"
