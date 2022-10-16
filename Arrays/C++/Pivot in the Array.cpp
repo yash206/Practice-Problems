a9cb23528e8c12077ff15f7a275182794a103993
aryananand27 <108974142+aryananand27@users.noreply.github.com>
2022-10-16 15:58:38 +0530

    Update Pivot in the Array.cpp

--- Arrays/C++/Pivot in the Array.cpp
+++ Arrays/C++/Pivot in the Array.cpp
@@ -1,3 +1,14 @@
+PROBLEM STATEMENT:
+
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the rightmost pivot index. If no such index exists, return -1.
+
+Here is code starts -->
+
 #include<iostream>
 using namespace std;
 int findpivot(int arr[],int n){
@@ -17,8 +28,11 @@ int findpivot(int arr[],int n){
     return mid;
 }
 int main(){
-int arr[5]={7,9,1,2,3};  //Sample input.
-int a=findpivot(arr,5);
-cout<<a<<endl;   // Output for sample input will be 2.
+int arr[5]={7,9,1,2,3};  // Sample Input 
+int a=findpivot(arr,5);   
+cout<<"The pivot in the array will be at index "a<<endl;    // Sample Output for the input will be at index 2.
 return 0;
 }
+
+ Sample Input                        Sample Output
+  {1,7,3,6,5,6}                       The pivot in the array will be at index 5
