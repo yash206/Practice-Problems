e1e8b27a62aec27f6398c624a8fd74b85b91524e
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2022-10-20 00:33:42 +0530

    Update Longest_Palindrome_Substring.cpp
    faster solution than the previous one in O(n^2) Time Complexity

--- Strings/C++/Longest_Palindrome_Substring.cpp
+++ Strings/C++/Longest_Palindrome_Substring.cpp
@@ -16,60 +16,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to print a substring str[low..high]
-void printSubStr(string str, int low, int high)
-{
-    for (int i = low; i <= high; ++i)
-        cout << str[i];
-}
-
-// This function prints the
-// longest palindrome substring
-// It also returns the length
-// of the longest palindrome
-int longestPalSubstr(string str)
-{
-    // get length of input string
-    int n = str.size();
-
-    // All substrings of length 1
-    // are palindromes
-    int maxLength = 1, start = 0;
-
-    // Nested loop to mark start and end index
-    for (int i = 0; i < str.length(); i++)
-    {
-        for (int j = i; j < str.length(); j++)
-        {
-            int flag = 1;
-
-            // Check palindrome
-            for (int k = 0; k < (j - i + 1) / 2; k++)
-                if (str[i + k] != str[j - k])
-                    flag = 0;
-
-            // Palindrome
-            if (flag && (j - i + 1) > maxLength)
-            {
-                start = i;
-                maxLength = j - i + 1;
+string longestPalindrome(string s1) {
+        if(s1.size() < 2)
+            return s1;
+        int k = 0, j = 0, i = 0, longest = 1, st = 0;
+        while(k < s1.size() - longest/2){
+            j = k;
+            while(s1[k] == s1[k+1])
+                ++k;
+            i = k;
+            while(k < s1.size() && j >0 && s1[k+1] == s1[j-1]){
+                ++k;
+                --j;
             }
+            if(k-j+1 > longest){
+                longest = k-j+1;
+                st = j;
+            }
+            k = i + 1;
         }
+        return s1.substr(st,longest);
     }
-
-    cout << "Longest palindrome substring is: ";
-    printSubStr(str, start, start + maxLength - 1);
-    cout << " and length is ";
-    // return length of LPS
-    return maxLength;
-}
 
 // Driver Code
 int main()
 {
     string str = "forgeeksskeegfor"; // Sample Input
-    cout << longestPalSubstr(str);
-    // Output :- Longest palindrome substring is: geeksskeeg and length is: 10 
+    cout << longestPalindrome(str);
+    // Output :- Longest palindrome substring is: geeksskeeg
 
     return 0;
 }
